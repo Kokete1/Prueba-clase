4d98333353c9e8e71e6979f87274ea732e20d971
Jorge <Kokete199@gmail.com>
2025-04-10 12:00:36 +0200

    Primer commit

---
+++ Passenger.java
@@ -0,0 +1,12 @@
+// Passenger.java
+class Passenger {
+    String name;
+    boolean isFrequentFlyer;
+    boolean hasDiscountCode;
+
+    public Passenger(String name, boolean isFrequentFlyer, boolean hasDiscountCode) {
+        this.name = name;
+        this.isFrequentFlyer = isFrequentFlyer;
+        this.hasDiscountCode = hasDiscountCode;
+    }
+}
